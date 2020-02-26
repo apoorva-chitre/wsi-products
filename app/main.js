d4a3956d82fa66c561b35652e93b91fb093c3618
Apoorva Chitre <apoorvac09@gmail.com>
2020-02-25 20:25:07 -0500

    update code to iterate and build product template from products json

--- app/main.js
+++ app/main.js
@@ -2,13 +2,51 @@
 
 import css from "./style.css";
 
-let products = [];
-
 fetch("../data.json")
 .then( response => { 
     return response.json()
 })
 .then ( data => {
-    products = data.groups;
+    const products = data.groups;
+
+    const productsTemplate = `
+        ${products.forEach( product => {
+            buildProduct(product)
+        })}
+    `;
+
+    console.log(productsTemplate);
+
+    document.getElementById('products').innerHTML = productsTemplate;
 });
 
+
+  
+function buildProduct(product) {
+
+    console.log(product.price);
+
+    var price = 0;
+
+    if(product.price.regular){
+        price = product.price.regular
+    } else {
+        if(product.price.selling.high) {
+            price = product.price.selling.high
+        } else {
+            price = product.price.selling
+        }
+    }
+
+    return `
+        <section id="product">
+            <img src="${product.hero.href}" width="${product.hero.width}" />
+            <h2>${product.name}</h2>
+            <aside>
+                <ul>
+                    <li>${price}</li>
+                </ul>
+            </aside>
+        </section>
+    `;  
+}
